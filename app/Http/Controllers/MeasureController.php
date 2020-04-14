846b28b2c35b5c7dcc58b763418a4f52086acff7
Salvador P <salvadorperezd@gmail.com>
2020-04-14 11:22:29 +0100

    [CHANGE] Removed the filter_var it was removing the dots. Investigate this.

--- app/Http/Controllers/MeasureController.php
+++ app/Http/Controllers/MeasureController.php
@@ -26,9 +26,9 @@ class MeasureController extends Controller
      {
         $measure = new Measure;
 
-       $measure->temperature= filter_var($request->temperature, FILTER_SANITIZE_NUMBER_FLOAT);
-       $measure->humidity = filter_var($request->humidity, FILTER_SANITIZE_NUMBER_FLOAT);
-       $measure->pressure = filter_var($request->pressure, FILTER_SANITIZE_NUMBER_FLOAT);
+       $measure->temperature= $request->temperature;
+       $measure->humidity = $request->humidity;
+       $measure->pressure = $request->pressure;
 
        $measure->save();
 
@@ -46,9 +46,9 @@ class MeasureController extends Controller
      {
         $measure= Measure::find($id);
 
-        $measure->temperature = filter_var($request->input('temperature'), FILTER_SANITIZE_NUMBER_FLOAT);
-        $measure->humidity = filter_var($request->input('humidity'), FILTER_SANITIZE_NUMBER_FLOAT);
-        $measure->pressure = filter_var($request->input('pressure'), FILTER_SANITIZE_NUMBER_FLOAT);
+        $measure->temperature = $request->input('temperature');
+        $measure->humidity = $request->input('humidity');
+        $measure->pressure = $request->input('pressure');
         $measure->save();
         return response()->json($measure);
      }
