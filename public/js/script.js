15715683ce7bd05a46f56aa2d6d3c12ea6a93070
Salvador P <salvadorperezd@gmail.com>
2021-02-02 15:56:28 +0100

    [FIX] Fixed a typo con colors.

--- public/js/script.js
+++ public/js/script.js
@@ -12,13 +12,13 @@ var backgroundChartColors = {
 	grey: 'rgb(201, 203, 207, 0.2)'
 };
 var chartColors = {
-    red: 'rgb(255, 99, 132',
-	orange: 'rgb(255, 159, 64',
-	yellow: 'rgb(255, 205, 86',
-	green: 'rgb(75, 192, 192',
-	blue: 'rgb(54, 162, 235',
-	purple: 'rgb(153, 102, 255',
-	grey: 'rgb(201, 203, 207'
+    red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
 };
 
 fetch(url)
